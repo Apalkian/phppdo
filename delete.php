a9439f96f4d6a98fb227080c07a0f86aa0749c98
Apalkian <Apalkian12@gmail.com>
2026-03-02 15:20:26 +0800

    changes

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@ require 'config.php';
 if (isset($_GET['delete'])) { 
     $users_id = $_GET['delete'];
 
-    $stmt = $pdo->prepare("DELETE FROM users WHERE user_id = ?");
+    $stmt = $pdo->prepare("DELETE FROM users WHERE users_id = ?");
     $stmt->execute([$users_id]);    
 
 }
