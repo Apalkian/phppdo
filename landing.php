b8e4c91518816d6ea04a7041b924c76061f0519c
Apalkian <Apalkian12@gmail.com>
2026-03-04 12:29:23 +0800

    updated

--- landing.php
+++ landing.php
@@ -28,23 +28,39 @@ if (isset($_GET['edit'])) {
 ?>
 
 <!-- ADD / UPDATE FORM -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <title>PHP FORM</title>
+ </head>
+ <body>
 
-<h3><?= $editUser ? 'Update User' : 'Add User' ?></h3>
+<h3 ><?= $editUser ? 'Update User' : 'Add User' ?></h3>
 
 <form method="POST">
   <?php if (!empty($editUser)): ?>
 
     <input type="hidden" name="users_id" value="<?= $editUser['users_id'] ?>">
   <?php endif; ?>
-
-  <label>Name:</label>
-  <input type="text" name="name" value="<?= !empty($editUser) ? $editUser['name'] : '' ?>" required><br>
-  <label>Email:</label>
-  <input type="email" name="email" value="<?= !empty($editUser) ? $editUser['email'] : '' ?>" required><br>
-  <label>Product:</label>
+  <div class="input group mb-3"></div>
+      <label>Name:</label>
+        <span class="input-group-text" name="name"></span>
+          <input type="text" name="name" value="<?= !empty($editUser) ? $editUser['name'] : '' ?>" required><br>
+      </div>
+    <div class="input group mb-3">
+        <label type="text" class="form-control" placeholder="Email">Email:</label>
+          <input type="email" name="email" value="<?= !empty($editUser) ? $editUser['email'] : '' ?>" required><br>
+    </div>
+    <div class="input-group">
+  <span class="input-group-text"><strong>Product</strong></span>
   <input type="text" name="product" placeholder="Product" required><br>
-  <label>Amount:</label>
-  <input type="number" step="0.01" name="amount" placeholder="Amount" required><br>
+  </div>
+  <div class="input-group mb-3">
+  <span class="input-group-text">$</span>
+  <input type="text" aria-label="Dollar amount" step="0.01" name="amount" placeholder="Amount" required><br>
+  </div>
 
   <!-- Submit buttons -->
 
@@ -62,22 +78,27 @@ if (isset($_GET['edit'])) {
 </form>
 <hr>
 <!-- USER TABLE -->
-<h3>User List</h3>
-<table border="1" cellpadding="10">
-
-  <tr>
-    <th>users_id</th>
-    <th>Name</th>
-    <th>Email</th>
-    <th>Action</th>
-  </tr>
-
+<h3 >User & Order List</h3>
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+     <th scope="col">users_id</th>
+    <th scope="col">Name</th>
+    <th scope="col">Email</th>
+    <th scope="col">Product</th>
+    <th scope="col">Amount</th>
+    <th scope="col">Action</th>
+    </tr>
+  </thead>
   <?php foreach ($users as $user): ?>
 
   <tr>
     <td><?= $user['users_id'] ?></td>
     <td><?= $user['name'] ?></td>
     <td><?= $user['email'] ?></td>
+    <td><?= $user['product'] ?></td>
+    <td><?= $user['amount'] ?></td>
+  
 
     <td>
 
@@ -88,3 +109,6 @@ if (isset($_GET['edit'])) {
   </tr>
   <?php endforeach; ?>
 </table>
+  
+ </body>
+ </html>
--- select.php
+++ select.php
@@ -1,6 +1,7 @@
 <?php
 require'config.php';
 
-$stmt =$pdo->query("SELECT * FROM users");
-$users =$stmt->fetchAll(PDO::FETCH_ASSOC);  
+$stmt =$pdo->query("SELECT users.users_id, users.name, users.email, orders.product, orders.amount FROM users
+ INNER JOIN orders ON users.users_id = orders.user_id");
+$users =$stmt->fetchAll(PDO::FETCH_ASSOC);   
 ?>
